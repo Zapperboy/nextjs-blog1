de2d0d7c5b29163fe9ae701328a7ce2ef7e51ad8
Zapperboy <vdpruben1@gmail.com>
2023-08-14 12:29:23 +0200

    fgf

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,51 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Minesweeper from '../components/Minesweeper'; // Import Minesweeper component here
+
+function getSortedPostsData() {
+  return [
+    { id: 1, date: '2023-07-31', title: 'Blog Post 1' },
+    { id: 2, date: '2023-07-30', title: 'Blog Post 2' },
+    // Add more blog posts here...
+  ];
+}
 
 export default function Home({ allPostsData }) {
   return (
-    <div>
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>Hii my name is Ruben</p>
-          <p>
-            See Me On <a href="https://www.instagram.com/_ruben_143/">instagram</a>.
-          </p>
-        </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>{title}</Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  {/* <Date dateString={date} /> */}
-                </small>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <Link href="/newpage">Play a quick game</Link>
-        <Link href="/newpage2"> and here is a new game</Link>
-        <Minesweeper />
-      </Layout>
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hii my name is Ruben</p>
+        <p>
+          See Me On
+          <a href="https://www.instagram.com/_ruben_143/"> instagram</a>.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                {/* Assuming you have a component called `Date` to format the date */}
+                {/* If not, replace this with your preferred method of displaying the date */}
+                {date}
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <Link href={`/newpage`}>Play a quick game</Link>
+
+      <Link href={`/newpage2`}> and here is a diffrent game</Link>
+    </Layout>
   );
 }
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
--- pages/newpage2.js
+++ pages/newpage2.js
@@ -1,12 +1,258 @@
 import React from 'react';
-import Minesweeper from '../components/Minesweeper'; // Import Minesweeper component here
+import PropTypes from 'prop-types';
 
-export default function newpage2({ allPostsData }) {
-  return (
-    <div>
-      <h1>New Page 2</h1>
-      {/* You can render your Minesweeper component here */}
-      <Minesweeper />
-    </div>
-  );
+class Cell extends React.Component {
+    getValue() {
+      const { value } = this.props;
+  
+      if (!value.isRevealed) {
+        return this.props.value.isFlagged ? '🚩' : null;
+      }
+      if (value.isMine) {
+        return '💣';
+      }
+      if (value.neighbour === 0) {
+        return null;
+      }
+      return value.neighbour;
+    }
+
+    render() {
+        const { value, onClick, cMenu } = this.props;
+        return (
+          <div onClick={this.props.onClick} onContextMenu={this.props.cMenu}>
+            {this.getValue()}
+          </div>
+        );
+      }
+    }
+
+  // Type checking With PropTypes
+const cellItemShape = {
+    isRevealed: PropTypes.bool,
+    isMine: PropTypes.bool,
+    isFlagged: PropTypes.bool,
+  };
+  
+  Cell.propTypes = {
+    value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
+    onClick: PropTypes.func,
+    cMenu: PropTypes.func,
+  };
+
+  class Board extends React.Component {
+  state = {
+    boardData: this.initBoardData(this.props.height, this.props.width, this.props.mines),
+    gameStatus: false,
+    mineCount: this.props.mines,
+  };
+  
+  render() {
+    return (
+      <div className="board">
+        <div className="game-info">
+          <span className="info">mines: {this.state.mineCount}</span>
+          <br />
+          <span className="info">{this.state.gameStatus}</span>
+        </div>
+        {this.renderBoard(this.state.boardData)}
+      </div>
+    );
+  }
 }
+
+
+  // Type checking With PropTypes
+  Board.propTypes = {
+    height: PropTypes.number,
+    width: PropTypes.number,
+    mines: PropTypes.number,
+  }
+
+  { createEmptyArray(height, width)
+    let data = [];
+    for (let i = 0; i < height; i++) {
+      data.push([]);
+      for (let j = 0; j < width; j++) {
+        data[i][j] = {
+          x: i,
+          y: j,
+          isMine: false,
+          neighbour: 0,
+          isRevealed: false,
+          isEmpty: false,
+          isFlagged: false,
+          };
+        }
+      }
+      return data;
+    }
+
+    { plantMines(data, height, width, mines)
+        let randomx, randomy, minesPlanted = 0;
+        while (minesPlanted < mines) {
+          randomx = this.getRandomNumber(width);
+          randomy = this.getRandomNumber(height);
+          if (!(data[randomx][randomy].isMine)) {
+            data[randomx][randomy].isMine = true;
+            minesPlanted++;
+          }
+        }
+        return (data);
+      }
+
+      { getNeighbours(data, height, width) 
+        let updatedData = data;
+        for (let i = 0; i < height; i++) {
+          for (let j = 0; j < width; j++) {
+            if (data[i][j].isMine !== true) {
+              let mine = 0;
+              const area = this.traverseBoard(data[i][j].x, data[i][j].y, data);
+              area.map(value => {
+                if (value.isMine) {
+                  mine++;
+                }
+              });
+              if (mine === 0) {
+                updatedData[i][j].isEmpty = true;
+              }
+              updatedData[i][j].neighbour = mine;
+            }
+          }
+        }
+        return (updatedData);
+        }
+        
+        // looks for neighbouring cells and returns them
+    { traverseBoard(x, y, data) 
+    const el = [];
+    //up
+    if (x > 0) {
+      el.push(data[x - 1][y]);
+    } 
+    //down
+    if (x < this.props.height - 1) {
+      el.push(data[x + 1][y]);
+    }
+    //left
+    if (y > 0) {
+      el.push(data[x][y - 1]);
+    }
+    //right
+    if (y < this.props.width - 1) {
+      el.push(data[x][y + 1]);
+    }
+    // top left
+    if (x > 0 && y > 0) {
+      el.push(data[x - 1][y - 1]);
+    }
+    // top right
+    if (x > 0 && y < this.props.width - 1) {
+      el.push(data[x - 1][y + 1]);
+    }
+    // bottom right
+    if (x < this.props.height - 1 && y < this.props.width - 1) {
+      el.push(data[x + 1][y + 1]);
+    }
+    // bottom left
+    if (x < this.props.height - 1 && y > 0) {
+      el.push(data[x + 1][y - 1]);
+    }
+    return el;
+  }
+
+  { initBoardData(height, width, mines) 
+    let data = this.createEmptyArray(height, width);
+    data = this.plantMines(data, height, width, mines);
+    data = this.getNeighbours(data, height, width);
+    
+    return data;
+  }
+
+  { renderBoard(data) 
+    return data.map((datarow) => {
+      return datarow.map((dataitem) => {
+        return (
+          <div 
+            key={dataitem.x * datarow.length + dataitem.y}>
+            <Cell
+              onClick={() => this.handleCellClick(dataitem.x, dataitem.y)}
+              cMenu={(e) => this.handleContextMenu(e, dataitem.x, dataitem.y)}
+              value={dataitem}
+            />
+  // This line of code adds a clearfix div after the last cell of each row.
+            {(datarow[datarow.length - 1] === dataitem) ? <div className="clear" /> : ""}
+          </div>
+        );
+      })
+    });
+  }
+
+  { handleCellClick(x, y) 
+    // check if revealed. return if true.
+    if (this.state.boardData[x][y].isRevealed ||    this.state.boardData[x][y].isFlagged) return null;
+  // check if mine. game over if true
+    if (this.state.boardData[x][y].isMine) {
+      this.setState({gameStatus: "You Lost."});
+      this.revealBoard();
+      alert("game over");
+    }
+    let updatedData = this.state.boardData;
+    
+    if (updatedData[x][y].isEmpty) {
+     updatedData = this.revealEmpty(x, y, updatedData);
+    }
+  if (this.getHidden(updatedData).length === this.props.mines) {
+     this.setState({gameStatus: "You Win."});
+     this.revealBoard();
+     alert("You Win");
+    }
+    this.setState({
+     boardData: updatedData,
+     mineCount: this.props.mines -this.getFlags(updatedData).length,
+     gameWon: win,
+    });
+  }
+
+  { revealEmpty(x, y, data) 
+    let area = this.traverseBoard(x, y, data);
+    area.map(value => {
+      if (!value.isFlagged && !value.isRevealed && (value.isEmpty || !value.isMine)) {
+        data[value.x][value.y].isRevealed = true;
+        if (value.isEmpty) {
+          this.revealEmpty(value.x, value.y, data);
+        }
+      }
+    });
+    return data;
+  }
+
+  { handleContextMenu(event, x, y) 
+    event.preventDefault();  // prevents default behaviour (i.e. right click menu on browsers.)
+    let updatedData = this.state.boardData;
+    let mines = this.state.mineCount;
+    let win = false;
+    // check if already revealed
+    if (updatedData[x][y].isRevealed) return;
+    if (updatedData[x][y].isFlagged) {
+      updatedData[x][y].isFlagged = false;
+      mines++;
+    } else {
+      updatedData[x][y].isFlagged = true;
+      mines--;
+    }
+    if (mines === 0) {
+      const mineArray = this.getMines(updatedData);
+      const FlagArray = this.getFlags(updatedData);
+      if (JSON.stringify(mineArray) === JSON.stringify(FlagArray)) {
+        this.revealBoard();
+        alert("You Win");
+      }
+    }
+    this.setState({
+      boardData: updatedData,
+      mineCount: mines,
+      gameWon: win,
+    });
+  }
+  export default Board;
